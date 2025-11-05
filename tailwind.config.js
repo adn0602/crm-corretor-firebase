93b459c193eacaf7e5482d8e78a64143d27edd88
Alexandre Nascimento <59940630+adn0602@users.noreply.github.com>
2025-11-05 07:38:22 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,3 +4,14 @@ export default {
     autoprefixer: {},
   },
 }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
