ff3ebf6bba90276449676579ddb242679c6ef53e
Alexandre Nascimento <59940630+adn0602@users.noreply.github.com>
2026-02-02 08:40:47 -0300

    Configure build settings in vite.config.js
    Increased chunk size warning limit and configured manual chunking for vendor libraries.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Aumentamos o limite para o aviso não aparecer mais
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        // Isso organiza as bibliotecas pesadas (como o Firebase) em um arquivo separado
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
